77a21b963b755463030d2bb491c1df7ded4c9272
wilsonsun <wilson.sun@hotmail.ca>
2016-03-16 22:19:15 -0400

    Instruction stuff

---
+++ Workingbuild/Assets/Instructiondisp.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Instructiondisp : MonoBehaviour {
+	public Text instructionText;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Player") {
+			if (gameObject.name == "InstructionWall") {
+				instructionText.text = "Tap the middle of the screen to open the door. If you still can't open, that means you trigger the tripwire, you have to get to the terminal on the right and play n-gram inorder to open the door";
+			} else {
+				instructionText.text = "Tap the middle of the screen to open the door";
+			}
+
+			yield return new WaitForSeconds (3.0f);
+			instructionText.text = "";
+			Destroy (gameObject);
+		}
+	}
+}
